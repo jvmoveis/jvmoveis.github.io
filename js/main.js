7cde8a706c94b175c14fe23a224a7080a9e1b3bb
Haran Camatti <hbcamatti1@gmail.com>
2016-07-15 18:14:22 -0300

    teste ab

--- js/main.js
+++ js/main.js
@@ -420,15 +420,15 @@ $(document).ready(function(){
 	]);
 	
 	// Preload the page with jPreLoader
-	// $('body').jpreLoader({
-	// 	splashID: "#jSplash",
-	// 	showSplash: true,
-	// 	showPercentage: true,
-	// 	autoClose: true,
-	// 	splashFunction: function() {
-	// 		$('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
-	// 	}
-	// });
+	$('body').jpreLoader({
+		splashID: "#jSplash",
+		showSplash: true,
+		showPercentage: true,
+		autoClose: true,
+		splashFunction: function() {
+			$('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
+		}
+	});
 	
 	BRUSHED.nav();
 	BRUSHED.mobileNav();
